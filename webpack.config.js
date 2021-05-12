339bc529e3d2f3fc3544be5c9f3debda0064bdbb
unknown <mayapatil121198@gmail.com>
2021-05-12 13:54:38 +0530

    webpack plugins sass-loader css-loader

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const currentTask=process.env.npm_lifecycle_event;
+const path=require('path')
+const MiniCssExtractPlugin=require('mini-css-extract-plugin');
+const config={
+    entry:'./app/app.js',
+    output:{
+        filename:'mybundle.[hash].js',
+        path:path.resolve(__dirname,'dist')
+    },
+    // watch:true
+    plugins:[],
+    mode:"development",
+    devServer:{port:8080,
+    contentBase:path.resolve(__dirname,"dist")
+    },
+    module:{
+        rules:[
+            {
+                test:/\.scss$/,
+                use:['style-loader','css-loader','sass-loader'],
+            },
+            {
+
+                test:/\.js$/,
+                exclude:/(node_modules|bower_components)/,
+                use:{
+                    loader:"babel-loader",
+                    options:{
+                        presets:['@babel/preset-env','@babel/preset-react']
+                    }
+                }
+
+            }
+        ]
+    }
+    
+}
+if(currentTask=="build"){
+    config.mode="production";
+    config.module.rules[0].use[0]=MiniCssExtractPlugin.loader
+    config.plugins.push(new MiniCssExtractPlugin({filename:'main.[hash].css'}))
+}
+module.exports=config
